1beaf3e9066c7da7fd7e1814d15e86016689e0c2
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-05-05 04:56:24 +1000

    .

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
-// chrome.commands.onCommand.addListener((shortcut) => {
-//   console.log("lets reload");
-//   console.log(shortcut);
-//   if (shortcut.includes("+M")) {
-//     chrome.runtime.reload();
-//   }
-// });
+listenReloadCommand();
+function listenReloadCommand() {
+  chrome.commands.onCommand.addListener((shortcut) => {
+    console.log("lets reload");
+    console.log(shortcut);
+    if (shortcut.includes("+M")) {
+      chrome.runtime.reload();
+    }
+  });
+}
+console.log("hello background");
 
 chrome.tabs.onDetached.addListener(handleDetached);
 
